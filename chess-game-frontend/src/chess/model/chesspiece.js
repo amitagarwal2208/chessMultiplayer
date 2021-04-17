41aa80d59bb1489f0fe2b2768ce131cdc368feac
Amit Agarwal <amit.agarwal2208@gmail.com>
2021-04-17 16:27:31 +0530

    created ChessPiece template

---
+++ chess-game-frontend/src/chess/model/chesspiece.js
@@ -0,0 +1,21 @@
+class ChessPiece {
+    constructor(name, isAttacked, color, id) {
+        this.name = name // string
+        this.isAttacked = isAttacked // boolean
+        this.color = color // string
+        this.id = id // string
+    }
+
+    setSquare(newSquare) {
+        // set the square this piece is sitting top of. 
+        // on any given piece (on the board), there will always be a piece on top of it. 
+        // console.log(newSquare)
+    }
+
+    getSquare() {
+        return this.squareThisPieceIsOn
+    }
+}
+
+
+export default ChessPiece
