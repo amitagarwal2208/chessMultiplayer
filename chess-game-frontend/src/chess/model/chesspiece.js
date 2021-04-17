f1e0453157ca3386d86e5ee872d3c42b53188fcb
Amit Agarwal <amit.agarwal2208@gmail.com>
2021-04-17 16:49:36 +0530

    Square class template

--- chess-game-frontend/src/chess/model/chesspiece.js
+++ chess-game-frontend/src/chess/model/chesspiece.js
@@ -10,6 +10,24 @@ class ChessPiece {
         // set the square this piece is sitting top of. 
         // on any given piece (on the board), there will always be a piece on top of it. 
         // console.log(newSquare)
+        if (newSquare === undefined) {
+            this.squareThisPieceIsOn = newSquare
+            return 
+        }
+
+        if (this.squareThisPieceIsOn === undefined) {
+            this.squareThisPieceIsOn = newSquare
+            newSquare.setPiece(this)
+        }
+
+        const isNewSquareDifferent = this.squareThisPieceIsOn.x != newSquare.x || this.squareThisPieceIsOn.y != newSquare.y
+
+        if (isNewSquareDifferent) {
+            // console.log("set")
+            this.squareThisPieceIsOn = newSquare
+            newSquare.setPiece(this)
+        }
+
     }
 
     getSquare() {
